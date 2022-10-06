3a3446c12a4454a9edca26ade6a947fbbbbf8f35
Sam Rundel <samuel.rundel@live.de>
2022-10-06 02:32:01 -0700

    working implementation of batch gradient regression

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import skeleton
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from sklearn.datasets import load_boston
+from sklearn.metrics import mean_squared_error
 
 path = "/home/sam/Downloads/IA1_train.csv"
 
@@ -6,8 +11,11 @@ path = "/home/sam/Downloads/IA1_train.csv"
 def task0():
     data = skeleton.load_data(path)
     data = skeleton.preprocess_data(data, True, True)
-    print(data)
-
+    weight, loses = skeleton.gd_train(data, ["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors",
+                                             "waterfront", "view", "condition", "grade", "sqft_above", "sqft_basement",
+                                             "yr_built", "yr_renovated", "zipcode", "lat", "long", "sqft_living15",
+                                             "sqft_lot15"], 0.05, 2500)
+    skeleton.plot_losses(loses)
 
 def task1():
     data = skeleton.load_data(path)
@@ -17,4 +25,4 @@ def task1():
 
 if __name__ == '__main__':
     task0()
-    task1()
+    # task1()
--- skeleton.py
+++ skeleton.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
-from matplotlib import plotlib as plt
+import plotly.express as px
 
 
 def load_data(path):
     # Your code here:
-    return pd.read_csv(path)
+    rawdata = pd.read_csv(path)
+    data = rawdata.apply(pd.to_numeric, errors='coerce')
+    return data
+
 
 # Implements dataset preprocessing, with boolean options to either normalize the data or not, 
 # and to either drop the sqrt_living15 column or not.
@@ -14,48 +17,52 @@ def load_data(path):
 # / aren't normalized, or versions that have / lack sqrt_living15.
 def preprocess_data(data, normalize, drop_sqrt_living15):
     # Your code here:
+    return data
 
-    return preprocessed_data
 
 # Implements the feature engineering required for part 4. Quite similar to preprocess_data.
 # Expand the arguments of this function however you like to control which feature modification
 # approaches are / aren't active.
 def modify_features(data):
     # Your code here:
+    modified_data = ""
 
     return modified_data
+
+
+def loss_function(training_values, target, weights):
+    predictions = np.dot(training_values, weights.T)
+    return (1 / len(target)) * np.sum((predictions - target) ** 2)
+
+
+def batch_gradient_descent(training_values, target, weights, learning_rate, iterations):
+    loss_history = [0] * iterations
+    for i in range(iterations):
+        prediction = np.dot(training_values, weights.T)
+        weights = weights - (learning_rate / len(target)) * np.dot(prediction - target, training_values)
+        loss_history[i] = loss_function(training_values, target, weights)
+    return weights, loss_history
+
 
 # Trains a linear model on the provided data and labels, using the supplied learning rate.
 # weights should store the per-feature weights of the learned linear regression.
 # losses should store the sequence of MSE losses for each epoch of training, which you will then plot.
-def gd_train(data, labels, lr):
-    # Your code here:
+def gd_train(data, labels, lr, iterations):
+    training_values = data[labels]
+    target = data['price']
+    weights = np.zeros(training_values.shape[1])
+    print("Start-Weight: " + str(loss_function(training_values, target, weights)))
+    training_values = (training_values - training_values.min()) / (training_values.max() - training_values.min())
+    batch_weight, batch_loss_history = batch_gradient_descent(training_values, target, weights, lr, iterations)
 
-    return weights, losses
+    print("End-Theta: " + str(loss_function(training_values, target, batch_weight)))
+
+    return batch_weight, batch_loss_history
+
 
 # Generates and saves plots of the training loss curves. Note that you can interpret losses as a matrix
 # containing the losses of multiple training runs and then put multiple loss curves in a single plot.
 def plot_losses(losses):
-    # Your code here:
-
+    fig = px.line(losses, x=range(len(losses)), y=losses, labels={'x': 'no. of iterations', 'y': 'cost function'})
+    fig.show()
     return
-
-# Invoke the above functions to implement the required functionality for each part of the assignment.
-
-# Part 0  : Data preprocessing.
-# Your code here:
-
-
-# Part 1 . Implement batch gradient descent and experiment with different learning rates.
-# Your code here:
-
-
-# Part 2 a. Training and experimenting with non-normalized data.
-# Your code here:
-
-
-# Part 2 b Training with redundant feature removed. 
-# Your code here:
-
-
-
